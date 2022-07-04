4647c5a4c8eb1f8a21753ac676ea50badf16533a
Oussama Mangoub <mangoub4@gmail.com>
2022-07-04 08:03:55 +0100

    API UPDATE

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import SignPDF from "./SignPDF";
+import fs from "node:fs";
+import path from "node:path";
+
+async function main() {
+  const pdfBuffer = new SignPDF(
+    path.resolve("test_assets/minions.pdf"),
+    path.resolve("test_assets/identity.p12")
+  );
+
+  const signedDocs = await pdfBuffer.signPDF();
+  const randomNumber = Math.floor(Math.random() * 5000);
+  const pdfName = `./exports/exported_file_${randomNumber}.pdf`;
+
+  fs.writeFileSync(pdfName, signedDocs);
+  console.log(`New Signed PDF created called: ${pdfName}`);
+}
+
+main();
